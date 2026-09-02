919778e61f874560b26438c8216045ea3358a538
Ana <anajulia.vieira.tds24@gmail.com>
2026-09-02 17:12:38 -0300

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 
-
 function App() {
+
+  let nome = "Naju"
+  let sobrenome = "Vieira"
+  let nome_completo = nome + " " + sobrenome
+
+  let cliques = 0 // comando do react que monitora a variável para quando ela precisa ser renderizada/atualizada
+  // cliques += useState(0)
+
+  function aumentarClique(){
+    cliques += 1
+    
+  }
 
   return (
     <div>
       <h1>Olá mundo!</h1>
       <p>Estou aprendendo React C:</p>
+      <p>Meu nome é {nome_completo}.</p>
+
+      <hr />
+
+      <p>Você clicou {cliques} vezes.</p>
+      <button onClick={aumentarClique}>Clique aqui!</button>
+
     </div>
   )
 }
